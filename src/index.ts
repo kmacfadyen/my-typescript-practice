5a76a30a4ab70c91dbdbfaa3924ca5018dce201e
Kevin MacFadyen <kmacfadyen3@gmail.com>
2024-02-22 15:52:42 -0500

    checkpoint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// import { v4 as uuidV4 } from 'uuid';
+import { v4 as uuidV4 } from 'uuid';
 import confetti from 'canvas-confetti';
 
 confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
@@ -6,6 +6,21 @@ confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
   useWorker: true,
 })({ particleCount: 200, spread: 200 });
 
-// console.log(uuidV4());
 
-const list = document.querySelector("#list");
+const list = document.querySelector<HTMLUListElement>("#list");
+const form = document.querySelector("new-task-form") as HTMLFormElement | null;
+const input = document.querySelector<HTMLInputElement>("#new-task-title");
+
+
+form?.addEventListener("submit", e => {
+  e.preventDefault();
+
+  if (input?.value == "" || input?.value == null) return 
+
+  const task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  }
+})
